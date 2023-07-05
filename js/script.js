bf10a05bea324649956d789f7ea7a39ade90aefe
gertz50 <138722483+gertz50@users.noreply.github.com>
2023-07-06 01:15:21 +0200

    ordnerstruktur
    html, css und js als eigene dateien

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var clicked = false;
+
+function evilMoment(event) {
+  if (!clicked) {
+    document.body.style.backgroundImage = "url('pics/hintergrund2.gif')";
+    var audio = new Audio('sounds/roundstart.mp3');
+    audio.play();
+    clicked = true;
+  }
+  event.stopPropagation();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var redirectLink = document.getElementById('redirect-link');
+  redirectLink.addEventListener('click', function(event) {
+    event.stopPropagation();
+    window.location.href = 'https://yungztr.de';
+  });
+
+  var button = document.querySelector('.button');
+  button.addEventListener('click', function(event) {
+    event.stopPropagation();
+    window.location.href = 'ww.html';
+  });
+
+  document.body.addEventListener('click', evilMoment);
+});
