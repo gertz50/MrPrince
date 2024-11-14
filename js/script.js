c7cb1d4664b8e264da0f940cb5908a518cdd1a50
gertz50 <138722483+gertz50@users.noreply.github.com>
2024-11-14 02:26:47 +0100

    scheiss drauf das passwort wird in clear text gespeichert

--- js/script.js
+++ js/script.js
@@ -1,28 +1,11 @@
 // Funktion, um die Antwort zu überprüfen
-console.log(process.env.RIDDLEANSWER1);
-console.log(process.env.RIDDLEANSWER1);
-console.log(process.env.RIDDLEANSWER1);
-console.log(process.env.RIDDLEANSWER1);
-console.log(process.env.RIDDLEANSWER1);
-console.log(process.env.RIDDLEANSWER1);
-console.log(process.env.RIDDLEANSWER1);
-console.log(process.env.RIDDLEANSWER1);
-console.log(process.env.RIDDLEANSWER1);
-function checkAnswer(answerId, contentId, riddleNr, errorId) {
+function checkAnswer(answerId, contentId, correctAnswer, errorId) {
     const userAnswer = document.getElementById(answerId).value.trim();
     const errorMessage = document.getElementById(errorId);
-    let correctAnswer = "";
-    console.log(process.env.RIDDLEANSWER1);
-
-    if (riddleNr == 1) {
-        correctAnswer = process.env.RIDDLEANSWER1 || "";
-        console.log(process.env.RIDDLEANSWER1);
-    }
 
     if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
         // Zeige den Inhalt an
         document.getElementById(contentId).style.display = 'block';
-        console.log(process.env.RIDDLEANSWER1);
         // Verstecke das Textfeld und das Rätsel
         document.getElementById(answerId).parentElement.style.display = 'none';
     } else {
@@ -30,26 +13,20 @@ function checkAnswer(answerId, contentId, riddleNr, errorId) {
 
         // Zeige die Fehlermeldung bei einer falschen Antwort an
         errorMessage.style.display = 'block';
-        console.log(process.env.RIDDLEANSWER1);
     }
 }
 
 // Ereignislistener für Enter-Taste hinzufügen
-function addEnterKeyListener(answerId, contentId, riddleNr, errorId) {
-    console.log(process.env.RIDDLEANSWER1);
+function addEnterKeyListener(answerId, contentId, correctAnswer, errorId) {
     const answerInput = document.getElementById(answerId);
     answerInput.addEventListener('keypress', function(event) {
         if (event.key === 'Enter') {
-            checkAnswer(answerId, contentId, riddleNr, errorId);
-            console.log(process.env.RIDDLEANSWER1);
+            checkAnswer(answerId, contentId, correctAnswer, errorId);
         }
     });
 }
 
-console.log(process.env.RIDDLEANSWER1);
-
 // Listener beim Laden der Seite für jedes Rätsel hinzufügen
 document.addEventListener('DOMContentLoaded', function() {
-    console.log(process.env.RIDDLEANSWER1);
-    addEnterKeyListener('answer1', 'content1', 1, 'error1');
+    addEnterKeyListener('answer1', 'content1', 'jason', 'error1');
 });
