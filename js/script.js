ce60df3ffbcf3ac3c1a55e5ac8a0c471f8ab5879
gertz50 <138722483+gertz50@users.noreply.github.com>
2024-11-14 03:39:28 +0100

    Revert "final test"
    This reverts commit 01ae92fa6caccc40df3b3c7150dcdf0c4df7feb3.

--- js/script.js
+++ js/script.js
@@ -1,54 +1,36 @@
 // Funktion, um die Antwort zu überprüfen
-
-console.log(process.env.RIDDLEANSWER1);
-
-function checkAnswer(answerId, contentId, riddleNr, errorId) {
+function checkAnswer(answerId, contentId, correctAnswer, errorId) {
     const userAnswer = document.getElementById(answerId).value.trim();
     const errorMessage = document.getElementById(errorId);
-
-    let correctAnswer = "";
-    console.log(process.env.RIDDLEANSWER1);
-
-    if (riddleNr == 1) {
-        correctAnswer = process.env.RIDDLEANSWER1 || ""; // Falls process.env.RIDDLEANSWER1 undefined ist, setze es auf einen leeren String
-        console.log(correctAnswer);
-        console.log(process.env.RIDDLEANSWER1);
-    }
-
 
     if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
         // Zeige den Inhalt an
         document.getElementById(contentId).style.display = 'block';
         // Verstecke das Textfeld und das Rätsel
         document.getElementById(answerId).parentElement.style.display = 'none';
-        console.log(process.env.RIDDLEANSWER1);
     } else {
         // alert('Falsche Antwort! Versuche es nochmal.');
 
         // Zeige die Fehlermeldung bei einer falschen Antwort an
         errorMessage.style.display = 'block';
-        console.log(process.env.RIDDLEANSWER1);
     }
-
-    console.log(process.env.RIDDLEANSWER1);
 }
 
 // Ereignislistener für Enter-Taste hinzufügen
-function addEnterKeyListener(answerId, contentId, riddleNr, errorId) {
+function addEnterKeyListener(answerId, contentId, correctAnswer, errorId) {
     const answerInput = document.getElementById(answerId);
-    console.log(process.env.RIDDLEANSWER1);
 
     if (answerInput) {
         answerInput.addEventListener('keypress', function(event) {
             if (event.key === 'Enter') {
-                checkAnswer(answerId, contentId, riddleNr, errorId);
+                checkAnswer(answerId, contentId, correctAnswer, errorId);
             }
         });
     }
 
     answerInput?.addEventListener('keypress', function(event) {
         if (event.key === 'Enter') {
-            checkAnswer(answerId, contentId, riddleNr, errorId);
+            checkAnswer(answerId, contentId, correctAnswer, errorId);
         }
     });
     
@@ -56,6 +38,5 @@ function addEnterKeyListener(answerId, contentId, riddleNr, errorId) {
 
 // Listener beim Laden der Seite für jedes Rätsel hinzufügen
 document.addEventListener('DOMContentLoaded', function() {
-    addEnterKeyListener('answer1', 'content1', 1, 'error1');
-    console.log(process.env.RIDDLEANSWER1);
+    addEnterKeyListener('answer1', 'content1', 'jason', 'error1');
 });
